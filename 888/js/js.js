bb3dd1c827c453642bed8899bbd9905965b2edad
Klimenko Nazar <nazarchik>
2018-09-13 17:13:12 +0300

    init commit

---
+++ 888/js/js.js
@@ -0,0 +1,12 @@
+// window.onscroll = function() {
+// 	docScroll();
+// }
+
+// function docScroll () {
+// 	let windowScroll - document.body.scrollTop || document.documentElement.scrollTop,
+// 		documentHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+// 		scrolled = (windowScroll / documentHeight) * 100;
+
+// 		document.getElementById('logo').style.width = scrolled + '%';
+// }
+
